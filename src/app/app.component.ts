6541f71971e5346dfcdcc7a7fd7cc63b8ead265d
Rishav Sharma <rsrishav0@gmail.com>
2023-03-09 16:37:50 +0900

    initial base code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isCool: boolean,
+  isStylish: boolean
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string = "Image source Example: Google Photos";
+  imgSrc: string = "https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA=w240-h480-rw";
+
+  myName: string = "Rish"
+
+  pokemons: Pokemon[] = [{
+    id: 1,
+    name: "pokemon",
+    type: "electric",
+    isCool: false,
+    isStylish: true
+  }, {
+    id: 2,
+    name: "squirtle",
+    type: "water",
+    isCool: true,
+    isStylish: false
+  },{
+    id: 3,
+    name: "charmander",
+    type: "fire",
+    isCool: false,
+    isStylish: false
+  }]
+
+  pokemonName: string = "";
+
+  constructor(){
+  }
+
+  handleClick(value :any) {
+    console.log(value.value);
+  }
+
+  handleChange(event: any) {
+    this.pokemonName = event.target.value;
+  }
+
 }
---
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonListComponent,
+    PokemonDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PokemonListComponent
+  ]
+})
+export class PokemonBaseModule { }
